4317b9c6d3d0d4426d3734496ebef451788e6e14
22003619 <22003619@myrp.edu.sg>
2023-05-24 11:42:13 +0800

    first push to P00 in github

---
+++ P00/test/HelloWorldTest.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: 22003619
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Wednesday 24-05-2023 00:12
+ */
+
+/**
+ * @author 22003619
+ *
+ */
+public class HelloWorldTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		//fail("Not yet implemented");
+        System.out.println("This is the testcase for HelloWorld");
+        String str1="This is the testcase for HelloWorld";
+        String str2="This is the testcase for HelloWorld";
+        assertEquals(str1, str2);
+
+	}
+
+}
